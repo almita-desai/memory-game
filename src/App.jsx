d6e16099318da9a97830e7a3c54b75b901f06661
almita-desai <almita.desai@gmail.com>
2025-05-30 19:07:18 +0530

    Add shuffle logic and turn counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
-import { useState } from 'react'
+import React, { useDeferredValue, useRef, useState } from 'react';
 
+const cardImages=[
+  {"src":"/src/assets/img/img0.png"},
+  {"src":"/src/assets/img/img1.png"},
+  {"src":"/src/assets/img/img2.png"},
+  {"src":"/src/assets/img/img3.png"},
+  {"src":"/src/assets/img/img4.png"},
+  {"src":"/src/assets/img/img5.png"},
+  {"src":"/src/assets/img/img6.png"},
+  {"src":"/src/assets/img/img7.png"},
+  {"src":"/src/assets/img/img8.png"},
+  {"src":"/src/assets/img/img9.png"}
+]
 
-function App() {
+function App(){
+  const [card, setCard] = useState([]);
+  const [turn, setTurn] = useState(0);
+
+  const shuffleCards=()=>{
+  const shuffledCards=[...cardImages,...cardImages]
+  .sort(()=>Math.random()-0.5)
+  .map((card)=>({...card,id:Math.random()}))
+  setCard(shuffledCards)
+  setTurn(turn+1)
+  }
+  console.log(card,turn)
+  return(
+    <>
+    <div className='flex flex-col justify-center items-center'>
+      <h1 className='font-bold text-2xl m-4 '>Memory Game</h1>
+      <button onClick={shuffleCards} className='button'> 
+        New Game
+      </button>
+    </div>
+    </>
+  )
 }
 
 export default App
