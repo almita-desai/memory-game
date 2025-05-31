0d6b66965403faa3c2d14a8a38723332856fcdbf
almita-desai <almita.desai@gmail.com>
2025-05-31 23:01:47 +0530

    Add timer

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,8 @@ function App(){
   const [choiceOne, setChoiceOne] = useState(null);
   const [choiceTwo, setChoiceTwo] = useState(null);
   const [disable, setDisable] = useState(false);
+  const [isActive, setIsActive] = useState(false);
+  const [timer, setTimer] = useState(0);
 
   const shuffleCards=()=>{
   const shuffledCards=[...cardImages,...cardImages]
@@ -32,6 +34,8 @@ function App(){
   .map((card)=>({...card,id:Math.random()}))
   setCard(shuffledCards)
   setTurn(0)
+  setIsActive(true)
+  setTimer(0)
   }
   const handleChoice=(card)=>{
     if (choiceOne==null){
@@ -79,15 +83,36 @@ function App(){
  useEffect(() => {
   shuffleCards();
 }, []);
+ 
+useEffect(() => {
+  let interval
+  if(isActive){
+    interval=setInterval(() => {
+      setTimer((prev)=>prev+1)
+    }, 1000);
+  }
+  return () => clearInterval(interval)
+}, [isActive]);
+
+useEffect(() => {
+  if(card.every(card=> card.matched)){
+    setIsActive(false)
+  }
+  
+}, [card]);
 
   return(
     <>
     <div className='flex flex-row justify-start h-screen'>
       <div className='flex flex-col left-0 w-[360px] items-center rounded-r-md' style={{ backgroundColor: "#3f185b" }}>
-      <h1 className='text-4xl m-7 '>Memory Game</h1>
+      <h1 className='text-4xl m-7'>Memory Game</h1>
       <button onClick={shuffleCards} className='button'> 
-      New Game
+      Start
       </button>
+      <p className='text-2xl font-bold m-7'>Turns : {turn}</p>
+      <p className="font-semibold text-lg m-7">Time: {String(Math.floor(timer / 60)).padStart(2, '0')}:
+        {String(timer % 60).padStart(2, '0')}
+      </p>          
       </div>
     
     <div className='grid grid-cols-6 gap-0 p-7 mx-54 my-10'>
